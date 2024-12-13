c7d8b48ca9fa4f816a1cb695f3133a2f7ba106e1
Faiga91 <faiga91@gmail.com>
2024-12-13 12:57:10 +0100

    Refactor Sphinx configuration for improved readability and consistency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,34 +1,30 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+Configuration file for the Sphinx documentation builder.
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+-- Project information -----------------------------------------------------
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
 
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
-project = 'ViT-Flexible-Heads'
-copyright = '2024, Faiga Alawad'
-author = 'Faiga Alawad'
+sys.path.insert(0, os.path.abspath("../../"))
+PROJECT = "ViT-Flexible-Heads"
+COPYRIGHT = "2024, Faiga Alawad"
+AUTHOR = "Faiga Alawad"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+HTML_THEME = "alabaster"
+html_static_path = ["_static"]
