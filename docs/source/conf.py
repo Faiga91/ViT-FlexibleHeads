58c9557956b5cea95650b8ee49e76a79e4120a2b
Faiga91 <faiga91@gmail.com>
2024-12-13 12:59:57 +0100

    Add Sphinx Napoleon extension for Google-style docstring support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ AUTHOR = "Faiga Alawad"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
